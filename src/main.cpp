bf846e2347190ad17fc7b77924049fce6ad7f859
John Afaghpour <jafaghpo@student.42.fr>
2019-10-28 23:30:42 +0100

    added click event in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,16 @@ int main(int argc, char** argv)
 		while (!quit)
 		{
 			SDL_WaitEvent(&event);
+
+			if (event.button.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT && event.button.windowID == 1)
+			{
+				auto test = ui.pixel_to_index(Position { event.button.x, event.button.y });
+				ui.place_stone(1, Position { test.x, test.y });
+				ui.place_stone(1, Position { test.x + 1, test.y + 1 });
+				ui.render();
+			}
 			if (event.key.keysym.sym == SDLK_ESCAPE)
-			quit = true;
+				quit = true;
 		}
 	}
 	catch (std::exception &e)
