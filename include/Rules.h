fd6d882ac716df418faa6d1f2bd20bb4e0f5edc3
Guillaume Gregoire <ggregoir@student.42.fr>
2019-11-18 18:54:40 +0100

    added rules files

---
+++ include/Rules.h
@@ -0,0 +1,34 @@
+#pragma once
+
+class Rules
+{
+	private:
+
+	public:
+
+};
+
+
+class Classic : public Rules
+{
+	private:
+
+	public:
+
+};
+
+class Standard : public Rules
+{
+	private:
+
+	public:
+
+};
+
+class 4 : public Rules
+{
+	private:
+
+	public:
+
+};
