a07a5fac6c9f9d6d60420b2beaba0c2bef82cfb7
John Afaghpour <jafaghpo@student.42.fr>
2019-12-02 17:35:52 +0100

    separated UI from Menu in main

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@
 
 using namespace std;
 
-void	print_stones(GameManager &manager, uint8_t stone)
+// FOR DEBUG
+static void	print_stones(GameManager &manager, uint8_t stone)
 {
 	auto list = manager.board.get_stones(stone);
 	cout << "[";
@@ -22,46 +23,44 @@ void	print_stones(GameManager &manager, uint8_t stone)
 	cout << "]" << endl;
 }
 
+static void	run_ui(Parameters params)
+{
+	GameManager		manager;
+	UserInterface	ui(params);
+	SDL_Event		event;
+	bool			quit = false;
+
+	ui.render();
+	while (!quit)
+	{
+		SDL_WaitEvent(&event);
+		if (LEFT_CLICK(event))
+		{
+			auto new_stone = ui.pixel_to_pos(Position(event.button.x, event.button.y));
+			if (manager.modify_board(new_stone.index(), manager.get_turn_color() + 1))
+			{
+				print_stones(manager, manager.get_turn_color());
+				manager.change_turn();
+			}
+			else
+				cout << "Cannot add a stone here" << endl;
+			ui.print_board(manager.get_board(), manager.get_last_move());
+			ui.render();
+		}
+		if (CLOSE_EVENT(event))
+		{
+			cout << "ESCAPE KEY PRESSED" << endl;
+			quit = true;
+		}
+	}
+}
+
 int main(int ac, char **av) 
 {
 	Parameters		params;
 	Menu			menu;
-	GameManager		manager;
-	SDL_Event		event;
-	bool			quit = false;
 
 	if (ac == 1 || (strcmp(av[1], "--nomenu") && strcmp(av[1], "-n")))
 		menu.loop(params);
-	UserInterface	ui(params);
-	ui.render();
-	try
-	{
-		while (!quit)
-		{
-			SDL_WaitEvent(&event);
-
-			if (LEFT_CLICK(event))
-			{
-				auto new_stone = ui.pixel_to_pos(Position(event.button.x, event.button.y));
-				if (manager.modify_board(new_stone.index(), manager.get_turn_color() + 1))
-				{
-					print_stones(manager, manager.get_turn_color());
-					manager.change_turn();
-				}
-				else
-					cout << "Cannot add a stone here" << endl;
-				ui.print_board(manager.get_board(), manager.get_last_move());
-				ui.render();
-			}
-			if (CLOSE_EVENT(event))
-			{
-				cout << "ESCAPE KEY PRESSED" << endl;
-				quit = true;
-			}
-		}
-	}
-	catch (std::exception &e)
-	{
-		quit = true;
-	}
+	run_ui(params);
 }
